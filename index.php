a8737f70f4b7a65fa263254ee6a826ce6c7642b6
SlavkaVavrinova <slavka.vav@gmail.com>
2021-11-21 15:34:09 +0100

    start

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Kontakty</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <div id="wrapper">
+      <!-- SideBar -->
+      <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+          <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+              <div class="dropdown profile-element">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                  <span class="block m-t-xs font-bold">Example user</span>
+                  <span class="text-muted text-xs block"
+                    >menu <b class="caret"></b
+                  ></span>
+                </a>
+                <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                  <li><a class="dropdown-item" href="login.html">Logout</a></li>
+                </ul>
+              </div>
+              <div class="logo-element">IN+</div>
+            </li>
+            <li class="active">
+              <a href="#"
+                ><i class="fa fa-th-large"></i>
+                <span class="nav-label">Kontakty</span></a
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- / End SideBar -->
+
+      <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+          <!-- Top bar -->
+          <nav
+            class="navbar navbar-static-top white-bg"
+            role="navigation"
+            style="margin-bottom: 0"
+          >
+            <div class="navbar-header">
+              <a
+                class="navbar-minimalize minimalize-styl-2 btn btn-primary"
+                href="#"
+                ><i class="fa fa-bars"></i>
+              </a>
+            </div>
+          </nav>
+        </div>
+        <!-- /End Top bar -->
+
+        <!-- Main content -->
+        <div class="wrapper wrapper-content animated fadeInRight">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="contact-box">
+                <a class="row" href="#">
+                  <div class="col-4">
+                    <div class="text-center">
+                      <img
+                        alt="image"
+                        class="rounded-circle m-t-xs img-fluid"
+                        src="https://source.unsplash.com/128x128/?woman"
+                      />
+                      <div class="m-t-xs font-bold">Graphics designer</div>
+                    </div>
+                  </div>
+                  <div class="col-8">
+                    <h3><strong>Sandra Smith</strong></h3>
+                    <address>
+                      <i class="fa fa-map-marker"></i> Riviera State 32/106
+                    </address>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="contact-box">
+                <a class="row" href="#">
+                  <div class="col-4">
+                    <div class="text-center">
+                      <img
+                        alt="image"
+                        class="rounded-circle m-t-xs img-fluid"
+                        src="https://source.unsplash.com/128x128/?woman"
+                      />
+                      <div class="m-t-xs font-bold">PHP Programmer</div>
+                    </div>
+                  </div>
+                  <div class="col-8">
+                    <h3><strong>Janet Carton</strong></h3>
+                    <address>
+                      <i class="fa fa-map-marker"></i> Riviera State 32/106
+                    </address>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="contact-box">
+                <a class="row" href="#">
+                  <div class="col-4">
+                    <div class="text-center">
+                      <img
+                        alt="image"
+                        class="rounded-circle m-t-xs img-fluid"
+                        src="https://source.unsplash.com/128x128/?woman"
+                      />
+                      <div class="m-t-xs font-bold">Marketing manager</div>
+                    </div>
+                  </div>
+                  <div class="col-8">
+                    <h3><strong>Monica Zimber</strong></h3>
+                    <address>
+                      <i class="fa fa-map-marker"></i> Riviera State 32/106
+                    </address>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /End Main content -->
+
+        <!-- Footer -->
+        <footer class="footer">
+          <div class="text-center">
+            <strong>Copyright</strong> Example Company &copy; 2014-2019
+          </div>
+        </footer>
+        <!-- /End Footer -->
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="assets/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/inspinia.js"></script>
+    <!-- /End Scripts -->
+  </body>
+</html>
